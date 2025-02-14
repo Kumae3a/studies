231ba26f34161c5588b0867fd8e256515bb9fc30
blokey <blokey.kr@gmail.com>
2025-02-14 10:56:43 +0700

    spring one commit

---
+++ spring_course/src/main/java/com/mycompany/spring_course/spring_introduction/Cat.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.spring_course.spring_introduction;
+
+
+public class Cat implements Pet {
+
+    @Override
+    public void say() {
+        System.out.println("Meow-Meow");
+    }
+    
+    
+}
---
+++ spring_course/src/main/java/com/mycompany/spring_course/spring_introduction/Dog.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.spring_course.spring_introduction;
+
+/**
+ *
+ * @author blokey
+ */
+public class Dog implements Pet {
+    @Override
+    public void say(){
+        System.out.println("Bouw-Wou");
+    }
+    
+}
